ab498e2c336c8dc7636ab91ca94d4cdd98715386
Luperus11 <nawakhun.ka.64@ubu.ac.th>
2026-09-24 02:15:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def generate_slides_from_groq(topic_or_content):
         }
         
         payload = {
-            "model": "llama-3.3-70b-versatile",
+            "model": "llama-3.1-8b-instant",
             "messages": [
                 {"role": "user", "content": prompt}
             ],
